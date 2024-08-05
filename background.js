07b2999ea77ad8c752af8a4f13fc64c0eb239df0
Seth Cottle <SethCottle@gmail.com>
2024-08-05 15:51:20 -0400

    Simplified sanitation approach

--- background.js
+++ background.js
@@ -16,53 +16,18 @@ chrome.action.onClicked.addListener((tab) => {
 });
 
 function sanitizeAndUpdateUrl(tab) {
-  const url = new URL(tab.url);
+  let url = new URL(tab.url);
   
-  // List of common tracking parameters to remove
-  const trackingParams = [
-    // UTM parameters
-    'utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content',
-    // Social media
-    'fbclid', 'gclid', 'msclkid', 'igshid',
-    // Email marketing
-    'mc_eid', 'mc_cid',
-    // General
-    'source', 'medium', 'campaign', 'content', 'term',
-    'ref', 'referrer', 'ref_src', 'ref_url',
-    'share', 'share_source',
-    // Analytics
-    '_ga', '_gl', '_hsenc', '_hsmi', 'yclid', 'yp_c',
-    // E-commerce and affiliate
-    'tag', 'affiliate', 'affiliateid', 'aff', 'afid',
-    'pcrid', 'productid', 'keywordid', 'siteid', 'adid', 'network',
-    // Amazon specific
-    'ascsubtag', 'sc', 'linkId', 'linkCode', 'camp', 'creative',
-    // Other e-commerce
-    'geniuslink', 'pd_rd_r', 'pd_rd_w', 'pd_rd_wg', 'pf_rd_p', 'pf_rd_r',
-    // Add any other parameters you want to remove
-  ];
-
-  // Remove all listed tracking parameters
-  trackingParams.forEach(param => {
-    url.searchParams.delete(param);
-  });
-
-  // Remove any parameter that starts with 'utm_'
-  for (let param of url.searchParams.keys()) {
-    if (param.startsWith('utm_') || param.startsWith('aff_')) {
-      url.searchParams.delete(param);
-    }
-  }
+  // Remove everything after '?'
+  url.search = '';
   
   // Remove ref parameters from the pathname
   let newPathname = url.pathname.replace(/\/ref\/.*$/, '');
   newPathname = newPathname.replace(/\/ref=.*$/, '');
   url.pathname = newPathname;
 
-  // Remove hash if it contains tracking information
-  if (url.hash.includes('?') || trackingParams.some(param => url.hash.includes(param))) {
-    url.hash = '';
-  }
+  // Remove hash
+  url.hash = '';
   
   const sanitizedUrl = url.toString();
   
